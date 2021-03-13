46efa3d4877be97564f322e4685d724f193db296
Artur Sponchiado <artursponchi@gmail.com>
2021-03-12 23:31:24 -0300

    💄 better notifications (admin) UI

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -3,11 +3,21 @@
     <div class="container text-left mt-3">
 
       <h4>Últimas {{ notificationResultLimit }} notificações</h4>
-      <div v-for="noti in notifications" :key="noti.id" class="card bg-dark mb-2">
+
+      <div v-if="isLoading.notifications" class="text-center mt-4">
+        <div class="spinner-border text-success" style="width: 3rem; height: 3rem;" role="status">
+          <span class="sr-only">Carregando...</span>
+        </div>
+      </div>
+
+      <div v-else v-for="noti in notifications" :key="noti.id" class="card bg-dark mb-2">
         <div class="card-body">
           <b>{{ noti.data().notificationTitle }}</b>
-          <div>{{ noti.data().notificationBody }}</div>
+          <br>
+          <code>{{ noti.data().notificationBody }}</code>
           <div>
+            <small>Para: {{ noti.data().targetUserName }}</small>
+            <br>
             <small class="text-muted">{{ noti.data().createdAt.toDate().toLocaleString() }}</small>
           </div>
         </div>
@@ -46,11 +56,15 @@ export default {
 
   methods: {
     async fetchNotifications () {
+      this.isLoading.notifications = true
+
       const data = await this.db.collection('notifications')
         .orderBy('createdAt', 'desc')
         .limit(this.notificationResultLimit)
         .get()
       this.notifications = data.docs
+
+      this.isLoading.notifications = false
     }
   }
 }
